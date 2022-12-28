100ae9f95f384f16ebb740d96121675760771f8d
robert.caspari <robert.caspari@live.de>
2022-12-28 11:46:07 +0100

    restructuring

--- azure_infra/modules/databricks/main.tf
+++ azure_infra/modules/databricks/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_resource_group" "rg-adb" {
 }
 
 resource "azurerm_storage_account" "data-lake" {
-  name                     = "sahr${var.project}${var.environment}"
+  name                     = "sahr${var.project}adb${var.environment}"
   resource_group_name      = azurerm_resource_group.rg-adb.name
   location                 = azurerm_resource_group.rg-adb.location
   account_tier             = "Standard"
